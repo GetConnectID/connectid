0af0ef0615653655adf89872f957a7ce7f61b561
ConnectID <brixit.officialcoin@outlook.com>
2026-09-14 20:27:41 +0200

    Add backend configuration example

---
+++ app/backend/config.example.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'db_host' => 'YOUR_DATABASE_HOST',
+    'db_name' => 'YOUR_DATABASE_NAME',
+    'db_user' => 'YOUR_DATABASE_USER',
+    'db_password' => 'YOUR_DATABASE_PASSWORD'
+];
