19d4f57a670f8e9bdb5c78636f8be1d07cc4c12e
ConnectID <brixit.officialcoin@outlook.com>
2026-09-14 20:53:13 +0200

    Change messages link from HTML to PHP

--- app/home.php
+++ app/home.php
@@ -248,7 +248,7 @@ $walletStatus = $user['wallet_address']
 
         <nav class="nav">
             <a href="home.php" class="active">Home</a>
-            <a href="messages.html">Messages</a>
+            <a href="messages.php">Messages</a>
             <a href="connections.html">Connections</a>
             <a href="communities.html">Communities</a>
             <a href="profile.php">Profile</a>
