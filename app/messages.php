a51d1b68d38d1c2c6a268a301c49de534ff092dd
ConnectID <brixit.officialcoin@outlook.com>
2026-09-26 08:38:30 +0200

    Debug messages PHP error

--- app/messages.php
+++ app/messages.php
@@ -1,5 +1,9 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
 require_once __DIR__ . '/backend/auth.php';
 require_once __DIR__ . '/backend/db.php';
 require_once __DIR__ . '/backend/csrf.php';
